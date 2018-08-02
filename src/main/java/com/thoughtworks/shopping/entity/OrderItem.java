85f7c3ab851b3766320a3d87c96aadc3b13e71bc
yifu <yifu@thoughtworks.com>
2018-08-02 19:19:47 +0800

    [fuyi] feat : add order item object

---
+++ src/main/java/com/thoughtworks/shopping/entity/OrderItem.java
@@ -0,0 +1,50 @@
+package com.thoughtworks.shopping.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int count;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @Column(name = "order_id")
+    private Long orderId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}

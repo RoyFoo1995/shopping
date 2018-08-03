5dddeac77ec77064d996fc879d781071e28b26cb
yifu <yifu@thoughtworks.com>
2018-08-03 09:41:38 +0800

    [fuyi] refactor : modify post method to put

--- src/main/java/com/thoughtworks/shopping/controller/OrderItemController.java
+++ src/main/java/com/thoughtworks/shopping/controller/OrderItemController.java
@@ -6,10 +6,7 @@ import com.thoughtworks.shopping.service.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/orders")
@@ -22,7 +19,7 @@ public class OrderItemController {
         this.orderItemService = orderItemService;
     }
 
-    @PostMapping("/{id}/orderItems")
+    @PutMapping("/{id}/orderItems")
     public ResponseEntity update(@PathVariable("id") Long orderId,@RequestBody OrderRequest orderRequest){
         if (orderItemService.update(orderId,orderRequest)){
             return ResponseEntity.accepted().build();
